cc27dd3bf0110a3e64ed155c82ec56de09dccf61
abhinavxox <abhinavxox@gmail.com>
2023-06-07 12:22:11 +0530

    <Added>:[Socket Programming]

---
+++ SocketProgramming/client.c
@@ -0,0 +1,25 @@
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+int sockfd;
+struct sockaddr_in serv_addr;
+bzero ((char*)&serv_addr, sizeof (serv_addr));
+serv_addr.sin_family = AF_INET;
+serv_addr.sin_addr.s_addr = inet_addr ("127.0.0.1");
+serv_addr.sin_port = htons (2345);
+sockfd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
+connect (sockfd, (struct sockaddr*)&serv_addr, sizeof (serv_addr));
+char data[100] = {0};
+read (sockfd, data, 3);
+printf ("%s", data);
+write (sockfd, "hello", 5);
+close (sockfd);
+exit (0);
+return 0;
+}
